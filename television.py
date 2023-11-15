0f06af4f229e42aa5e9280fca6d11dfa85bb7f7a
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2023-11-15 16:06:32 -0600

    Create television.py

---
+++ television.py
@@ -0,0 +1,29 @@
+class Television: 
+  MIN_VOLUME = 0
+  MAX_VOLUME = 2
+  MIN_CHANNEL = 0
+  MAX_CHANNEL = 3
+  
+  def __init__(self): 
+    self.__status = False
+    self.__muted = False
+    self.__volume += MIN_VOLUME
+    self.__channel += MIN_CHANNEL
+
+  def power(self): 
+
+  def mute(self): 
+
+  def channel_up(self): 
+
+  def channel_down(self): 
+
+  def volume_up(self): 
+
+  def volume_down(self): 
+
+  def __str__(self): 
+
+    
+
+
