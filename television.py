46d359407353ad12672ad88f0a88c563f7c016cc
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2023-11-15 16:19:37 -0600

    Update television.py

--- television.py
+++ television.py
@@ -12,14 +12,24 @@ class Television:
 
   def power(self): 
 
+    return self.__status
   def mute(self): 
 
+    return self.__muted
   def channel_up(self): 
-
+    if self.__channel == MAX_CHANNEL: 
+      self.__channel += MIN_CHANNEL
+    else: 
+      self.__channel += 1
+    return self.__channel
   def channel_down(self): 
 
   def volume_up(self): 
-
+    if self.__volume == MAX_VOLUME: 
+      self.__volume += MIN_VOLUME
+    else: 
+      self.__volume += 1
+    return self.__volume
   def volume_down(self): 
 
   def __str__(self): 
