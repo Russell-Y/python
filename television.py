2f4bc892f4209311c1383a44d6f7783285a3c1b8
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2023-11-17 19:36:42 -0600

    Update television.py

--- television.py
+++ television.py
@@ -1,24 +1,40 @@
 class Television: 
+  '''
+  Default values and variables, all values type -> int. 
+  '''
   MIN_VOLUME = 0
   MAX_VOLUME = 2
   MIN_CHANNEL = 0
   MAX_CHANNEL = 3
   
   def __init__(self): 
+    '''
+    Set all variables to private. 
+    '''
     self.__status = False
     self.__muted = False
     self.__volume = MIN_VOLUME
     self.__channel = MIN_CHANNEL
 
   def power(self): 
+    '''
+    Changes status variable to True when activating power on tv. 
+    '''
     self.__status = True
     return self.__status
   
   def mute(self): 
+    '''
+    Changes volume to mute (True) with muted variable when activated. 
+    '''
     self.__muted = True
     return self.__muted
   
   def channel_up(self): 
+    #All channel variables type -> int. 
+    '''
+    Cycles through all channels like an actual tv when constantly activated. 
+    '''
     if self.__channel == MAX_CHANNEL: 
       self.__channel = MIN_CHANNEL
     else: 
@@ -26,6 +42,10 @@ class Television:
     return self.__channel
   
   def channel_down(self): 
+    #All channel variables type -> int. 
+    '''
+    Switches to maximum channel when (descending) beyond the minimum channel, parentheses replace double quotation to express. 
+    '''
     if self.__channel == MIN_CHANNEL: 
       self.__channel = MAX_CHANNEL
     else: 
@@ -33,6 +53,10 @@ class Television:
     return self.__channel
     
   def volume_up(self): 
+    #All volume variables type -> int. 
+    '''
+    Stays at maximum volume like an actual tv when constantly activated. 
+    '''
     if self.__volume == MAX_VOLUME: 
       self.__volume = MAX_VOLUME
     else: 
@@ -40,6 +64,10 @@ class Television:
     return self.__volume
   
   def volume_down(self): 
+    #All volume variables type -> int. 
+    '''
+    Stays at minimum volume like an actual tv when constantly activated. 
+    '''
     if self.__volume == MIN_VOLUME: 
       self.__volume = MIN_VOLUME
     else: 
@@ -47,6 +75,9 @@ class Television:
     return self.__volume
     
   def __str__(self): 
+    '''
+    Returns all variables and values to display when activated. 
+    '''
     return f'Power = [{status}], Channel = [{channel}], Volume = [{volume}]'
     
 
